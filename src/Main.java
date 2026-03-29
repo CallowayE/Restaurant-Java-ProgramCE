8100410c3e4a86de9840783d0785a1a2d95ec9c0
CallowayE <143754528+CallowayE@users.noreply.github.com>
2026-03-29 13:13:06 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+
+        try {
+            scanRestaurantFile(restaurants);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        printRandomRestaurant(restaurants);
+
+    }
+
+    public static void scanRestaurantFile(ArrayList<Restaurant> list) throws FileNotFoundException {
+        File file = new File("src/restaurants.txt");
+        Scanner scan = new Scanner(file);
+        scan.useDelimiter(",");
+
+        while (scan.hasNext()) {
+            String name = scan.next();
+            int waitTime = scan.nextInt();
+            double rating = scan.nextDouble();
+            double avgPrice = scan.nextDouble();
+            double distance = scan.nextDouble();
+            String hoursOpen = scan.next();
+
+            if (rating > 5) {
+                rating = -1;
+            }
+
+            Restaurant restaurant = new Restaurant(name, waitTime, rating, avgPrice, distance, hoursOpen);
+
+            restaurants.add(restaurant);
+        }
+
+        printRandomRestaurant(restaurants);
+    }
+
+    public static void scanRestaurantFile(ArrayList<Restaurant> list){
+
+    }
+
+    /**
+     * Chooses a random Restaurant from an ArrayList and outputs the Restaurant
+     * @param list ArrayList of Restaurants
+     */
+    public static void printRandomRestaurant(ArrayList<Restaurant> list) {
+        Random randy = new Random();
+        int numRestaurants = list.size();
+        int randIndex = randy.nextInt(numRestaurants - 1);
+        System.out.println(list.get(randIndex));
+    }
+}
